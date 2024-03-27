37829214ff0ddb1c17f8908903107c48567351fc
f1aring <arman.rahman@g.bracu.ac.bd>
2024-03-28 03:57:07 +0600

    Third commit

--- src/database/pdf/pdf.controller.ts
+++ src/database/pdf/pdf.controller.ts
@@ -1,9 +1,8 @@
 import { Controller, Get, Post, Body, UploadedFiles ,UseInterceptors, Bind } from '@nestjs/common';
 import { PdfService } from './pdf.service';
 import { IPdf } from './pdf.interface';
-import { Express } from 'express'
 import { FilesInterceptor } from '@nestjs/platform-express';
-import {convertPDFBufferToImagesAndUpload} from './pdf.cloudinary'
+import {convertPDFBufferToImagesAndUpload} from './pdf.middleware'
 @Controller('pdf')
 export class PdfController {
   constructor(private readonly pdfService: PdfService) {}
@@ -12,29 +11,16 @@ export class PdfController {
   async getPdf(): Promise<any> {
     return await this.pdfService.findAll();
   }
-//   @Post('upload')
-//   @UseInterceptors(FileInterceptor('file'))
-  
-//    uploadFile(@UploadedFile() file: Express.Multer.File) {
-//      console.log(file);
-//    }
+
 @Post('upload')
 @UseInterceptors(FilesInterceptor('files'))
 @Bind(UploadedFiles())
     async uploadFile(files) {
-//   console.log(files[0].buffer);
-//   console.log(files[1].buffer);
-const pdf1 = await convertPDFBufferToImagesAndUpload(files[1].buffer);
+
+const pdf1 = await convertPDFBufferToImagesAndUpload(files[1].buffer);// this gives the array with the image link
 console.log(pdf1);
 }
 
-
-//    @Post('upload')
-//    @UseInterceptors(FilesInterceptor('files')) // Use FilesInterceptor for multiple files
-//    uploadFiles(@UploadedFiles() files: Express.Multer.File[]) {
-//      console.log(files);
-//      // Handle the uploaded files as needed
-//    }
 
   @Post()
   async postPdf(@Body() body: IPdf): Promise<IPdf> {
